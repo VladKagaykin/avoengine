2209d3ffe1e2ac19f4280c50eaedccc4f0cb64eb
AVCD <vladkagajkin233@gmail.com>
2026-08-18 12:35:32 +0300

    add key processing

--- window_processing.rs
+++ window_processing.rs
@@ -4,6 +4,13 @@ use winit::dpi::LogicalSize;
 use winit::window::WindowAttributes;
 use softbuffer::{Context, Surface};
 use std::num::NonZeroU32;
+use winit::keyboard::KeyCode;
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+thread_local! {
+    static PRESSED_KEYS: RefCell<HashMap<KeyCode, bool>> = RefCell::new(HashMap::new());
+}
 
 pub fn create_window(name: String) -> (Window, EventLoop<()>) {
     let settings = super::Engine_settings.lock().unwrap();
@@ -80,4 +87,30 @@ pub fn update_frame(window: &Window) -> Result<(), Box<dyn std::error::Error>> {
     
     buffer.present()?;
     Ok(())
+}
+
+pub fn update_key_state(key_code: KeyCode, pressed: bool) {
+    PRESSED_KEYS.with(|pressed_keys| {
+        let mut pressed_keys = pressed_keys.borrow_mut();
+        if pressed {
+            pressed_keys.insert(key_code, true);
+        } else {
+            pressed_keys.remove(&key_code);
+        }
+    });
+}
+
+pub fn get_pressed_keys(_window: &Window) -> Vec<KeyCode> {
+    PRESSED_KEYS.with(|pressed_keys| {
+        let pressed_keys = pressed_keys.borrow();
+        let mut result = Vec::new();
+        
+        for (key_code, &is_pressed) in pressed_keys.iter() {
+            if is_pressed {
+                result.push(key_code.clone());
+            }
+        }
+        
+        result
+    })
 }
